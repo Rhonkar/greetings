29ad5e081916bb3c252e7cd21c34a4e99d8d1dc3
Carsten Düllmann <cadull@microsoct.com>
2018-10-17 18:28:08 +0200

    docker-compose and web form

--- myapi/src/main/java/com/example/myapi/GreetingController.java
+++ myapi/src/main/java/com/example/myapi/GreetingController.java
@@ -4,16 +4,24 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final ArrayList<Greeting> stupidGreetingList = new ArrayList<Greeting>();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Greeting[] greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return stupidGreetingList.toArray(new Greeting[stupidGreetingList.size()]);
     }
+
+    @PostMapping("/greeting")
+    public String greetingPost(@ModelAttribute Greeting greeting) {
+        stupidGreetingList.add(greeting);
+        return "greeting";
+    }
+
 }
--- myapp/src/main/java/com/example/myapp/GreetingController.java
+++ myapp/src/main/java/com/example/myapp/GreetingController.java
@@ -3,15 +3,26 @@ package myapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greetingForm(@RequestParam(value="content", defaultValue="Default content") String content, Model model) {
+        Greeting greeting = new Greeting();
+        greeting.setId(1);
+        greeting.setContent(content);
+        model.addAttribute("greeting", greeting);
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greetings", new Greeting[] {greeting});
+        return "result";
     }
 
 }
